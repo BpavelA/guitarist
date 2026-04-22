f0cf24d2bfa55c2a28aeaf7062f62a5805019c04
BpavelA <x-man-pb@yandex.ru>
2026-04-22 09:54:56 +0300

    finished

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@
 
 let hamburger = document.querySelector(".hamburger");
 let mobileMenu = document.querySelector(".mobile-menu");
+let links = document.querySelectorAll(".mobile-menu a")
 
-hamburger.addEventListener("click", () => {
+function classToggler() {
  hamburger.classList.toggle("hamburger_active");
  mobileMenu.classList.toggle("active");
+};
+
+hamburger.addEventListener("click", classToggler)
+
+links.forEach(link => {
+ link.addEventListener("click", classToggler)
 })
